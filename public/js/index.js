f133d9dca44ef84a28035fae2d946cce6898f548
himanshush200599 <himanshush214@gmail.com>
2018-10-29 05:12:03 -0400

    emit message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,7 @@ var socket = io();
 
 socket.on('connect',function(){
   console.log('Connected to server');
-  socket.emit("createMessage",{
-    from:"himanshu",
-    text:"hello himanshu"
 
-  });
 });
 
 socket.on('disconnect',function(){
