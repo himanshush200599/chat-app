4f64b067ab33050687fb6a8e339ff3859e759846
himanshush200599 <himanshush214@gmail.com>
2018-10-30 13:20:25 -0400

    Added private chat rooms

--- server/server.js
+++ server/server.js
@@ -16,13 +16,13 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
   console.log('New user connected');
-  
+
   socket.on('join',(params,callback)=>{
     if(!isRealString(params.name) || !isRealString(params.room)){
       callback("Name and room name are required")
     }
 
-    socket.join(param.room);
+    socket.join(params.room);
     users.removeUser(socket.id)
     users.addUser(socket.id,params.name,params.room);
 
@@ -33,13 +33,18 @@ io.on('connection',(socket)=>{
     callback()
   })
   socket.on('createMessage',(message,callback)=>{
-    console.log("createMessage",message);
+    var user = users.getUser(socket.id);
 
-    io.emit('newMessage',generateMessage(message.from,message.text));
+    if(user && isRealString(message.text)){
+      io.to(user.room).emit('newMessage',generateMessage(user.name,message.text));
+
+
+    }
     callback('This is from server');
   })
 socket.on('createLocationMessage',(coords)=>{
-  io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude, coords.longitude))
+  var user = users.getUser(socket.id);
+  io.to(user.room).emit('newLocationMessage',generateLocationMessage(user.name,coords.latitude, coords.longitude))
 });
   socket.on('disconnect',()=>{
     var user = users.removeUser(socket.id);
