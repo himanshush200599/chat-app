06a926c44bbdb78a55dba2db7796cbc9ad9190d9
himanshush200599 <himanshush214@gmail.com>
2018-10-30 09:32:46 -0400

    added autoscrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+
+function scrollToButtonm(){
+  var messages = jQuery('#messages');
+  var newMessages = messages.children('li:last-child');
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var  scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessages.innerHeight();
+  var  lastMessageHeight = newMessages.prev().innerHeight();
+  if(clientHeight + scrollTop+lastMessageHeight+newMessageHeight >=scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+
+}
+
 socket.on('connect',function(){
   console.log('Connected to server');
 
@@ -18,6 +33,7 @@ socket.on('newMessage',function(message){
     createdAt:formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToButtonm();
   // var li = jQuery('<li></li>');
   // li.text(`${message.from} ${formattedTime}:${message.text}`);
   //
@@ -33,6 +49,7 @@ socket.on('newLocationMessage',function(message){
     url:message.url
   })
   jQuery('#messages').append(html);
+  scrollToButtonm();
   // var li = jQuery('<li></li>');
   // var a = jQuery('<a target="_blank">My current Location</a>');
   // li.text(`${message.from} ${formattedTime}:  `);
